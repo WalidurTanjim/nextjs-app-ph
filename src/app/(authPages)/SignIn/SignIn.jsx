5ecae1e5a54cff41ae78c6753ff4be17c016f12e
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-20 10:05:08 +0600

    AuthPages grouped route created

---
+++ src/app/(authPages)/SignIn/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SignInPage = () => {
+    return (
+        <div className='SignInPage'>
+            <div className="container mx-auto w-full px-6 lg:px-8 py-8">
+                <h1 className="text-center text-2xl text-slate-700 font-medium">Sign in page</h1>
+            </div>
+        </div>
+    );
+};
+
+export default SignInPage;
