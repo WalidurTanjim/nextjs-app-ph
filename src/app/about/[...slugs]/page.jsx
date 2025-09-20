d65da0a80143f0990dadc4be175d289c1d8a0e67
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-20 18:02:07 +0600

    catch-all segments

---
+++ src/app/about/[...slugs]/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutSlugPages = async({params}) => {
+    const URLParams = await params;
+    console.log(URLParams?.slugs)
+
+    return (
+        <div className='aboutSlugPages'>
+            <div className='container mx-auto w-full px-6 lg:px-8 py-8'>
+                <h1 className='text-center text-2xl font-medium text-slate-700'>About Slug Page</h1>
+            </div>
+        </div>
+    );
+};
+
+export default AboutSlugPages;
