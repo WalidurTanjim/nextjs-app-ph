ab619f57fc1ca675506cc35b41ae781bf98d987c
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-09-20 18:38:28 +0600

    loading file create

---
+++ src/app/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div className='loading'>
+            <div className="container mx-auto w-full px-6 lg:px-8 py-8">
+                <h1 className='text-center text-slate-700 text-2xl font-medium'>Loading...</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Loading;
